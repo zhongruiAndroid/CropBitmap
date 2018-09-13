f67496026a92d5b65000a9ab39a5c7313579c751
windows10 <2380253499@qq.com>
2018-09-13 20:39:54 +0800

    仿QQ头像裁剪

--- app/src/main/java/com/test/cropview/MainActivity.java
+++ app/src/main/java/com/test/cropview/MainActivity.java
@@ -25,10 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initView();
     }
-
     private void initView() {
         likeView=findViewById(R.id.likeView);
         sb=findViewById(R.id.sb);
