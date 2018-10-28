ec728c2d1ca3c030a063b077e643f3c161ddd332
windows10 <2380253499@qq.com>
2018-10-28 18:02:44 +0800

    仿小米图片裁剪-边框移动功能

--- MyCropBitmap/src/main/java/com/github/cropbitmap/LikeXMCropView.java
+++ MyCropBitmap/src/main/java/com/github/cropbitmap/LikeXMCropView.java
@@ -1,15 +1,35 @@
 package com.github.cropbitmap;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 /***
  *   created by zhongrui on 2018/10/24
  */
 public class LikeXMCropView extends View {
+    private void Log(String str){
+        Log.i("===","@@==="+str);
+    };
     private LikeXMCropViewUtils viewUtils;
+    private GestureDetector gestureDetector;
+
+    private int currentState=-1;
+    private final int touch_status_left=1;
+    private final int touch_status_top=2;
+    private final int touch_status_right=3;
+    private final int touch_status_bottom=4;
+    private final int touch_status_left_top=5;
+    private final int touch_status_right_top=6;
+    private final int touch_status_left_bottom=7;
+    private final int touch_status_right_bottom=8;
+
     public LikeXMCropView(Context context) {
         super(context);
         init(null);
@@ -26,13 +46,249 @@ public class LikeXMCropView extends View {
     }
 
     private void init(AttributeSet attrs) {
+        viewUtils=new LikeXMCropViewUtils();
+        //设置触摸边距
+        viewUtils.touchWidth=dip2px(15);
+        //设置裁剪框上下，左右间隔距离
+        viewUtils.borderDistance=viewUtils.touchWidth*2+dip2px(20);
+
+        gestureDetector=new GestureDetector(getContext(),new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                //X:左正右负,Y:上正下负
+                switch (currentState){
+                    case touch_status_left:
+                        //左边框向左移动
+                        if(distanceX>0){
+                            ////这个时候如果图片放大，超过左边部分，就需要缩小图片
+                            if(viewUtils.cropRect.left<=viewUtils.showBitmapRect.left){
+                                //这个时候就不能向左移动裁剪框了
+                            }else{
+                                //比较裁剪框和图片的左边距离是否小于移动距离
+                                float distance = Math.min(Math.abs(distanceX), Math.abs(viewUtils.cropRect.left - viewUtils.showBitmapRect.left));
+                                viewUtils.cropRect.left=viewUtils.cropRect.left-distance;
+                            }
+                        }else{
+                            //左边框向右移动
+                            //需要比较左边和右边的距离,保持左右两边的间隔
+                            if(viewUtils.cropRect.left+Math.abs(distanceX)+viewUtils.borderDistance>viewUtils.cropRect.right){
+                                float tempDistance = viewUtils.cropRect.right - viewUtils.borderDistance-viewUtils.cropRect.left;
+                                viewUtils.cropRect.left=viewUtils.cropRect.left+tempDistance;
+                            }else{
+                                viewUtils.cropRect.left=viewUtils.cropRect.left+Math.abs(distanceX);
+                            }
+                        }
+                    break;
+                    case touch_status_top:
+                        //上边框向上移动
+                        if(distanceY>0){
+                            ////这个时候如果图片放大，超过上边部分，就需要缩小图片
+                            if(viewUtils.cropRect.top<=viewUtils.showBitmapRect.top){
+
+                            }else{
+                                //比较裁剪框和图片的上边距离是否小于移动距离
+                                float distance = Math.min(Math.abs(distanceY), Math.abs(viewUtils.cropRect.top - viewUtils.showBitmapRect.top));
+                                viewUtils.cropRect.top=viewUtils.cropRect.top-distance;
+                            }
+                        }else{
+                            //上边框向下移动
+                            //需要保持上下两边的距离,保持上下的间隔
+                            if(viewUtils.cropRect.top+Math.abs(distanceY)+viewUtils.borderDistance>viewUtils.cropRect.bottom){
+                                float tempDistance =viewUtils.cropRect.bottom-viewUtils.borderDistance-viewUtils.cropRect.top;
+                                viewUtils.cropRect.top=viewUtils.cropRect.top+tempDistance;
+                            }else{
+                                viewUtils.cropRect.top=viewUtils.cropRect.top+Math.abs(distanceY);
+                            }
+                        }
+                    break;
+                    case touch_status_right:
+                        //右边框向右移动
+                        if(distanceX<0){
+                            ////这个时候如果图片放大，超过右边部分，就需要缩小图片
+                            if(viewUtils.cropRect.right>=viewUtils.showBitmapRect.right){
+
+                            }else{
+                                float tempDistance = Math.min(viewUtils.showBitmapRect.right - viewUtils.cropRect.right, Math.abs(distanceX));
+                                viewUtils.cropRect.right=viewUtils.cropRect.right+tempDistance;
+                            }
+                        }else{
+                            //右边框向左移动
+                            //需要比较左边和右边的距离,保持左右两边的间隔
+                            if(viewUtils.cropRect.left+viewUtils.borderDistance+Math.abs(distanceX)>viewUtils.cropRect.right){
+                                float tempDistance = viewUtils.cropRect.right - viewUtils.cropRect.left - viewUtils.borderDistance;
+                                viewUtils.cropRect.right=viewUtils.cropRect.right-tempDistance;
+                            }else{
+                                viewUtils.cropRect.right=viewUtils.cropRect.right-Math.abs(distanceX);
+                            }
+                        }
+                    break;
+                    case touch_status_bottom:
+                        //下边框向下移动
+                        if(distanceY<0){
+                            ////这个时候如果图片放大，超过下边部分，就需要缩小图片
+                            if(viewUtils.cropRect.bottom>=viewUtils.showBitmapRect.bottom){
+
+                            }else{
+                                float tempDistance = Math.min(viewUtils.showBitmapRect.bottom - viewUtils.cropRect.bottom, Math.abs(distanceY));
+                                viewUtils.cropRect.bottom=viewUtils.cropRect.bottom+tempDistance;
+                            }
+                        }else{
+                            //下边框向上移动
+                            //需要比较下边和上边的距离,保持下上两边的间隔
+                            if(viewUtils.cropRect.top+viewUtils.borderDistance+Math.abs(distanceY)>viewUtils.cropRect.bottom){
+                                float tempDistance = viewUtils.cropRect.bottom - viewUtils.cropRect.top - viewUtils.borderDistance;
+                                viewUtils.cropRect.bottom=viewUtils.cropRect.bottom-tempDistance;
+                            }else{
+                                viewUtils.cropRect.bottom=viewUtils.cropRect.bottom-Math.abs(distanceY);
+                            }
+                        }
+                    break;
+                    case touch_status_left_top:
+
+                    break;
+                    case touch_status_right_top:
+
+                    break;
+                    case touch_status_left_bottom:
+
+                    break;
+                    case touch_status_right_bottom:
+
+                    break;
+                }
+                viewUtils.refreshPath();
+                viewUtils.refreshTouchBorder(viewUtils.cropRect);
+                invalidate();
+//                return super.onScroll(e1, e2, distanceX, distanceY);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log("ACTION_DOWN");
+                if(viewUtils.leftBorderTouchRect.contains(event.getX(),event.getY())){
+                    /*左边框触摸*/
+                    currentState=touch_status_left;
+                }else if(viewUtils.topBorderTouchRect.contains(event.getX(),event.getY())){
+                    /*上边框触摸*/
+                    currentState=touch_status_top;
+
+                }else if(viewUtils.rightBorderTouchRect.contains(event.getX(),event.getY())){
+                    /*右边框触摸*/
+                    currentState=touch_status_right;
+
+                }else if(viewUtils.bottomBorderTouchRect.contains(event.getX(),event.getY())){
+                    /*下边框触摸*/
+                    currentState=touch_status_bottom;
+
+                }else if(viewUtils.leftTopTouchRect.contains(event.getX(),event.getY())){
+                    /*左上角触摸*/
+                    currentState=touch_status_left_top;
+
+                }else if(viewUtils.rightTopTouchRect.contains(event.getX(),event.getY())){
+                    /*右上角触摸*/
+                    currentState=touch_status_right_top;
+
+                }else if(viewUtils.leftBottomTouchRect.contains(event.getX(),event.getY())){
+                    /*左下角触摸*/
+                    currentState=touch_status_left_bottom;
+
+                }else if(viewUtils.rightBottomTouchRect.contains(event.getX(),event.getY())){
+                    /*右下角触摸*/
+                    currentState=touch_status_right_bottom;
+
+                }
+            break;
+            case MotionEvent.ACTION_UP:
+                currentState=-1;
+                Log("ACTION_UP");
+//                invalidate();
+            break;
+        }
+        gestureDetector.onTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
 
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        viewUtils=new LikeXMCropViewUtils();
+        viewUtils.centerX=w/2;
+        viewUtils.centerY=h/2;
+
+        viewUtils.viewWidth=w;
+        viewUtils.viewHeight=h;
+
+
+        viewUtils.initScale=1;
+        if(viewUtils.showBitmap.getWidth()<w&&viewUtils.showBitmap.getHeight()<h){
+            viewUtils.needMoveX=1f*(w-viewUtils.showBitmap.getWidth())/2;
+            viewUtils.needMoveY=1f*(h-viewUtils.showBitmap.getHeight())/2;
+        }else{
+            if(viewUtils.showBitmap.getWidth()*1f/viewUtils.showBitmap.getHeight()>w*1f/h){
+                viewUtils.initScale=w*1f/viewUtils.showBitmap.getWidth();
+                viewUtils.needMoveX=0;
+                viewUtils.needMoveY=1f*(h-viewUtils.showBitmap.getHeight()*viewUtils.initScale)/2;
+            }else{
+                viewUtils.initScale=h*1f/viewUtils.showBitmap.getHeight();
+                viewUtils.needMoveX=1f*(w-viewUtils.showBitmap.getWidth()*viewUtils.initScale)/2;
+                viewUtils.needMoveY=0;
+            }
+        }
+
         viewUtils.prepare();
+
+        viewUtils.showBitmapRect.set(0,0,viewUtils.showBitmap.getWidth(),viewUtils.showBitmap.getHeight());
+
+        viewUtils.showBitmapMatrix.postScale(viewUtils.initScale,viewUtils.initScale);
+        viewUtils.showBitmapMatrix.postTranslate(viewUtils.needMoveX,viewUtils.needMoveY);
+
+        viewUtils.showBitmapMatrix.mapRect(viewUtils.showBitmapRect);
+
+//        viewUtils.showBitmapMatrix.setRectToRect();
+
+        Log(viewUtils.showBitmapRect.toString());///、、、
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+        canvas.drawBitmap(viewUtils.showBitmap,viewUtils.showBitmapMatrix,null);
+
+        canvas.drawPath(viewUtils.cropPath,viewUtils.cropPaint);
+        canvas.drawPath(viewUtils.maskLayerPath,viewUtils.maskLayerPaint);
+
+        ///
+        canvas.drawRect(viewUtils.leftBorderTouchRect,viewUtils.cropPaint);
+        canvas.drawRect(viewUtils.topBorderTouchRect,viewUtils.cropPaint);
+        canvas.drawRect(viewUtils.rightBorderTouchRect,viewUtils.cropPaint);
+        canvas.drawRect(viewUtils.bottomBorderTouchRect,viewUtils.cropPaint);
+//
+        canvas.drawRect(viewUtils.leftTopTouchRect,viewUtils.cropPaint);
+        canvas.drawRect(viewUtils.rightTopTouchRect,viewUtils.cropPaint);
+        canvas.drawRect(viewUtils.leftBottomTouchRect,viewUtils.cropPaint);
+        canvas.drawRect(viewUtils.rightBottomTouchRect,viewUtils.cropPaint);
+        ///
+    }
+    public void setBitMap(final Bitmap bitmap){
+        viewUtils.showBitmap=bitmap;
+    }
+    private void prepare() {
+    }
+    private int dip2px(float dipValue) {
+        float scale = getContext().getResources().getDisplayMetrics().density;
+        return (int)(dipValue * scale + 0.5F);
     }
 }
--- MyCropBitmap/src/main/java/com/github/cropbitmap/LikeXMCropViewUtils.java
+++ MyCropBitmap/src/main/java/com/github/cropbitmap/LikeXMCropViewUtils.java
@@ -1,29 +1,46 @@
 package com.github.cropbitmap;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.util.Log;
 
 /***
  *   created by zhongrui on 2018/10/24
  */
 public class LikeXMCropViewUtils {
+
+
+    /*图片移动到view中心需要*/
+    public float needMoveX,needMoveY;
+    public float initScale=1;
+
+    /*裁剪框内部距离*/
+    public float borderDistance;
+
+
+    /*view中心坐标*/
+    public int centerX,centerY;
     /*图片*/
     public Bitmap showBitmap;
     public Matrix showBitmapMatrix;
+    public RectF showBitmapRect;
 
     /*裁剪框*/
-    public Matrix cropMatrix;
     public Paint cropPaint;
     public Path cropPath;
+    public RectF cropRect;
+    public Matrix cropMatrix;
     public int cropColor;
     public float cropWidth=2;
 
     /*裁剪框遮罩层*/
     public Paint maskLayerPaint;
     public Path  maskLayerPath;
+    public Matrix maskMatrix;
     public int maskLayerColor;
 
     /*裁剪框的四个角*/
@@ -41,31 +58,41 @@ public class LikeXMCropViewUtils {
     public float viewWidth;
     public float viewHeight;
 
- /*   public Path leftBorderTouchPath;
-    public Path topBorderTouchPath;
-    public Path rightBorderTouchPath;
-    public Path bottomBorderTouchPath;
+    public float touchWidth=30;
 
-    public Path leftTopTouchPath;
-    public Path rightTopTouchPath;
-    public Path leftBottomTouchPath;
-    public Path rightBottomTouchPath;*/
+    public RectF leftBorderTouchRect;
+    public RectF topBorderTouchRect;
+    public RectF rightBorderTouchRect;
+    public RectF bottomBorderTouchRect;
+
+    public RectF leftTopTouchRect;
+    public RectF rightTopTouchRect;
+    public RectF leftBottomTouchRect;
+    public RectF rightBottomTouchRect;
 
     public void prepare(){
-
+        //图片所在矩阵
+        showBitmapMatrix=new Matrix();
+        //图片所在矩形
+        showBitmapRect=new RectF();
         /*裁剪框*/
         cropMatrix=new Matrix();
         cropPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        cropPaint.setColor(Color.BLUE);
+        cropPaint.setStrokeWidth(2);
+        cropPaint.setStyle(Paint.Style.STROKE);
 
-
+        cropRect=new RectF();
         cropPath=new Path();
 
         /*裁剪框遮罩层*/
+        maskMatrix=new Matrix();
         maskLayerPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        maskLayerPaint.setColor(Color.parseColor("#30000000"));
+        maskLayerPaint.setStyle(Paint.Style.FILL);
 
 
         maskLayerPath=new Path();
-        maskLayerPath.addRect(new RectF(0,0,viewWidth,viewHeight), Path.Direction.CW);
 
         /*裁剪框的四个角*/
         borderAnglePaint=new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -82,8 +109,85 @@ public class LikeXMCropViewUtils {
 
 
         linePath=new Path();
+
+        int width = showBitmap.getWidth();
+        int height = showBitmap.getHeight();
+
+        leftBorderTouchRect =new RectF();
+        topBorderTouchRect =new RectF();
+        rightBorderTouchRect =new RectF();
+        bottomBorderTouchRect =new RectF();
+
+        leftTopTouchRect =new RectF();
+        rightTopTouchRect =new RectF();
+        leftBottomTouchRect =new RectF();
+        rightBottomTouchRect =new RectF();
+
+        /*显示图片区域path*/
+//        Path showBitmapPath=new Path();
+//        showBitmapPath.addRect();
+        initPath();
+        refreshTouchBorder(cropRect);
     }
 
+    public RectF getBitmapRect() {
+        if(showBitmapRect!=null){
+            showBitmapRect.setEmpty();
+        }else{
+            showBitmapRect=new RectF();
+        }
+        showBitmapRect.set(0,0,showBitmap.getWidth(),showBitmap.getHeight());
+
+        showBitmapMatrix.mapRect(showBitmapRect);
+        return showBitmapRect;
+    }
+    public void refreshTouchBorder(RectF cropRect) {
+        leftBorderTouchRect.set(cropRect.left,cropRect.top+touchWidth,cropRect.left+touchWidth,cropRect.bottom-touchWidth);
+        topBorderTouchRect.set(cropRect.left+touchWidth,cropRect.top,cropRect.right-touchWidth,cropRect.top+touchWidth);
+        rightBorderTouchRect.set(cropRect.right-touchWidth,cropRect.top+touchWidth,cropRect.right,cropRect.bottom-touchWidth);
+        bottomBorderTouchRect.set(cropRect.left+touchWidth,cropRect.bottom-touchWidth,cropRect.right-touchWidth,cropRect.bottom);
+
+        leftTopTouchRect.set(cropRect.left,cropRect.top,cropRect.left+touchWidth,cropRect.top+touchWidth);
+        rightTopTouchRect.set(cropRect.right-touchWidth,cropRect.top,cropRect.right,cropRect.top+touchWidth);
+        leftBottomTouchRect.set(cropRect.left,cropRect.bottom-touchWidth,cropRect.left+touchWidth,cropRect.bottom);
+        rightBottomTouchRect.set(cropRect.right-touchWidth,cropRect.bottom-touchWidth,cropRect.right,cropRect.bottom);
+    }
+    private void Log(String str){
+        Log.i("===","@@==="+str);
+    };
+    public void initPath(){
+        //裁剪框
+        if(!cropPath.isEmpty()){
+            cropPath.reset();
+        }
+        cropRect.set(needMoveX,needMoveY,viewWidth-1f*needMoveX,viewHeight-1f*needMoveY);
+        Log(cropRect.toString());///
+        cropPath.addRect(cropRect, Path.Direction.CW);
+
+        //遮罩层
+        if(!maskLayerPath.isEmpty()){
+            maskLayerPath.reset();
+        }
+        maskLayerPath.addRect(new RectF(0,0,viewWidth,viewHeight), Path.Direction.CW);
+        maskLayerPath.op(cropPath, Path.Op.XOR);
+
+    }
+    public void refreshPath() {
+        if(!cropPath.isEmpty()){
+            cropPath.reset();
+        }
+        cropPath.addRect(cropRect, Path.Direction.CW);
+
+        //遮罩层
+        if(!maskLayerPath.isEmpty()){
+            maskLayerPath.reset();
+        }
+        maskLayerPath.addRect(new RectF(0,0,viewWidth,viewHeight), Path.Direction.CW);
+        maskLayerPath.op(cropPath, Path.Op.XOR);
+    }
+    private void refreshPaint() {
+
+    }
     public void a(){
     }
 }
